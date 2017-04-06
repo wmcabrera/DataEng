27e86746c2db61124eeb955a151f0fa8f5bff9be
Wellington M. Cabrera <wcabrera@gmail.com>
2017-04-06 07:56:15 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,8 @@
 # the contents of this script could be replaced with similar files from any major language
 
 # I'll execute my programs, with the input directory log_input and output the files in the directory log_output
-python ./src/process_log.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
+#python ./src/process_log.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
 
+# Compile source code in src as g++ -std=c++11 process_log.cpp -o process_log 
+
+process_log ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
